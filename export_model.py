92dece8b8bbcab4efcf85405a004bca9e3453db9
ochwo laban <labanochwo@gmail.com>
2025-11-24 14:46:53 +0300

    tf1 to tf2

--- export_model.py
+++ export_model.py
@@ -35,7 +35,7 @@ class ModelExporter(object):
     with tf.Graph().as_default() as graph:
       self.inputs, self.outputs = self.build_inputs_and_outputs()
       self.graph = graph
-      self.saver = tf.train.Saver(tf.trainable_variables(), sharded=True)
+      self.saver = tf.compat.v1.train.Saver(tf.trainable_variables(), sharded=True)
 
   def export_model(self, model_dir, global_step_val, last_checkpoint):
     """Exports the model so that it can used for batch predictions."""
